bb0cd359d07187319aa7717788dfb5445df0c66f
Nicholas <nics-lohr@gmx.de>
2020-02-17 18:10:06 +0100

    more tests.

--- static_vector/test.cpp
+++ static_vector/test.cpp
@@ -13,6 +13,19 @@ struct throwing {
     }
 };
 
+// Allocating old-style function
+int* create_array(size_t s) {
+    return new int[s];
+}
+
+void use_array(int *, size_t ) {
+    // Do something...
+}
+
+void destroy_array(int * ptr) {
+    delete[] ptr;
+}
+
 int main() {
     vector<int> v(10, 0);
     vector<int> w(std::begin(v), std::end(v));
@@ -20,6 +33,13 @@ int main() {
     int *i = new int[10];
 
     vector<int, delete_allocator<int>> z(std::move(i), 10);
+
+    v = std::move(w);
+
+    vector<int, delete_allocator<int>> a(create_array(20), 20);
+    use_array(a.data(), a.size());
+    destroy_array(a.release());
+
 
     try {
         // Should not leak
